53c4849cc7e8585df3957c839ac9ddd38164cfaa
Farman <farman.khan@inmobi.com>
2020-12-14 01:41:26 +0530

    added wikkipedia and youtube support

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import speech_recognition as sr
 import pyttsx3 as tts
+import pywhatkit
+import datetime
+import wikipedia as wikki
 
 listener = sr.Recognizer()
 engine = tts.init()
@@ -32,11 +35,22 @@ def take_user_command():
 
 def process_command():
     command = take_user_command()
-    print("going to talk back the command...")
-    talk(command)
+
+    if 'play' or 'song' in command:
+        pywhatkit.playonyt(command)
+    elif 'time' or 'Time' in command:
+        time = datetime.datetime.now().strftime('%H:%M')
+        print(time)
+        talk("Current Time is " + time)
+
+    elif 'about' in command:
+        info = wikki.summary(command, 1)
+        print(info)
+        talk(info)
 
 
-process_command()
+while True:
+    process_command()
 
 
 
